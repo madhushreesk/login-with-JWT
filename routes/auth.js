8aaffd6693b3d37bb9dd3ce3b8e32eb925565810
madhushreesk <88674169+madhushreesk@users.noreply.github.com>
2023-09-11 12:16:47 +0530

    Added validation of user and email authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
+  // lets validate the data before we make a user
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // check if the user is already in database
+  const emailExists = await User.findOne({ email: req.body.email });
+  if (emailExists) return res.status(400).send("Email already exists");
+
+  // create new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
---
+++ validation.js
@@ -0,0 +1,26 @@
+const Joi = require("@hapi/joi");
+
+// Validation using joi
+
+// register validation
+
+const registerValidation = (data) => {
+  const schema = {
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  };
+  return Joi.validate(data, schema);
+};
+
+const loginValidation = (data) => {
+  const schema = {
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  };
+  return Joi.validate(data, schema);
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
